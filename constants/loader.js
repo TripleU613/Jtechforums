99f4257dd6bf703e3739c801b8fc091d8341c298
TripleU <tripleuworld@gmail.com>
2025-08-29 00:01:33 -0400

    Show logo overlay with progress bar

--- constants/loader.js
+++ constants/loader.js
@@ -1,4 +1,14 @@
+
+// Inject a fullscreen overlay with centered logo and a top progress bar.
 (function () {
+  const overlay = document.createElement('div');
+  overlay.id = 'loading-overlay';
+  const logo = document.createElement('img');
+  logo.src = '/img/whitelogo.png';
+  logo.alt = 'JTech logo';
+  overlay.appendChild(logo);
+  document.documentElement.appendChild(overlay);
+
   const bar = document.createElement('div');
   bar.id = 'loading-bar';
   document.documentElement.appendChild(bar);
@@ -18,9 +28,11 @@
     bar.style.width = '100%';
     setTimeout(() => {
       bar.style.opacity = '0';
+      overlay.style.opacity = '0';
     }, 200);
     setTimeout(() => {
       bar.remove();
+      overlay.remove();
     }, 500);
   });
 })();
